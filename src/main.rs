a165ddf05552d075d1dafed828c7c775d8ba6c9d
Corey Richardson <corey@octayn.net>
2017-06-24 02:40:46 -0400

    Read multiple inputs, precompute search tree

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 extern crate permutohedron;
-extern crate twoway;
 
-static WORDLIST : &[u8] = include_bytes!("wordlist");
+static WORDLIST : &str = include_str!("wordlist");
 
 use std::io::Write;
+use std::collections::BTreeSet;
 
 fn main() {
-    let mut buf = String::with_capacity(10);
-    std::io::stdin().read_line(&mut buf).expect("IO error reading input!");
     let stdout = std::io::stdout();
     let mut out = stdout.lock();
-    
-    let mut buf = buf.into_bytes();
-    for permutation in permutohedron::Heap::new(&mut buf) {
-        match twoway::find_bytes(WORDLIST, &permutation) {
-            Some(idx) => { out.write(&WORDLIST[idx .. idx+permutation.len()+1]); }
-            None => { }
+
+    let mut words = BTreeSet::new();
+    for word in WORDLIST.split('\n') {
+        words.insert(word);
+    }
+
+    loop {
+        out.write(b"> ");
+        out.flush();
+        let mut buf = String::with_capacity(10);  // I apologize
+        std::io::stdin().read_line(&mut buf).expect("IO error reading input!");
+        let mut buf = buf.trim().to_owned().into_bytes(); // I apologize even harder
+        
+        for permutation in permutohedron::Heap::new(&mut buf) {
+            if words.contains(unsafe { std::str::from_utf8_unchecked(&permutation) }.trim()) {
+                out.write(&permutation);
+                out.write(b"\n");
+            }
         }
     }
 }
